a1d7742cbc9c666b811455d7e9bdd806ff3e1a95
Petros Palioudakis <petros.palioudakis@mbww.com>
2022-12-06 11:54:19 +0200

    Add basic pages

---
+++ src/pages/ColorPicker.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ColorPicker = () => (
+  <div />
+);
+
+export default ColorPicker;
---
+++ src/pages/Customers.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Customers = () => (
+  <div />
+);
+
+export default Customers;
