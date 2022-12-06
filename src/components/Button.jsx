eaf3d4d9f87da7c4116614c50ff515f468d5b74c
Petros Palioudakis <petros.palioudakis@mbww.com>
2022-12-06 18:16:44 +0200

    Setup Ecommerce

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-const Button = () => (
-  <div>Button</div>);
+const Button = ({ text, color, bgColor, size, borderRadius }) => (
+  <button type="button" style={{ backgroundColor: bgColor, color, borderRadius }} className={`text-${size} p-3 hover:drop-shadow-xl`}>{text}</button>
+);
 
 export default Button;
--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
-const Ecommerce = () => (
-  <div>Ecommerce</div>);
+const Ecommerce = () => {
+  const p = 'text';
+  return <div>{p}</div>;
+};
 
 export default Ecommerce;
