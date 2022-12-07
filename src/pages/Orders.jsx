5399694b4479b9ce61f77e3c432577d70809cdd2
Petros Palioudakis <petros.palioudakis@mbww.com>
2022-12-07 18:10:24 +0200

    add header

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { GridComponent } from '@syncfusion/ej2-react-grids';
+import { Header } from '../components';
 
 const Orders = () => (
-  <div>Orders</div>
+  <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+    <Header category="Page" title="Orders" />
+    <GridComponent />
+  </div>
 );
 
 export default Orders;
